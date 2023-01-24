491be350c639d09f22d61ceea97bcb3f7a0f4f01
UmidaJamshidbekovna <78403650+UmidaJamshidbekovna@users.noreply.github.com>
2023-01-24 22:12:58 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+
+export default function App() {
+
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
